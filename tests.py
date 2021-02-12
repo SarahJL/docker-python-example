cad8a122fc8a0c270227869bfb8ffdfc7b088dae
Sarah Scott <sjlscott91@gmail.com>
2021-02-12 13:21:17 +0000

    second

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class TestHelloWorld2(unittest.TestCase):
 
     def test_hello_world(self):
         import app
-        self.assertEqual(app.hello_world(), "Hello, Worlds!")
+        self.assertEqual(app.hello_world(), "Hello, World!")
 
 if __name__ == '__main__':
     unittest.main()
