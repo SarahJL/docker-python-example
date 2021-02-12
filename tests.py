a3043f7f9953795c41054fdd66056401c25bbcd8
Sarah Scott <sjlscott91@gmail.com>
2021-02-12 13:50:56 +0000

    add my_sum testing

--- tests.py
+++ tests.py
@@ -1,11 +1,36 @@
 import unittest
 import app
 
+from fractions import Fraction
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+
+
 class TestHelloWorld(unittest.TestCase):
 
     def test_hello_world(self):
         import app
         self.assertEqual(app.hello_world(), "Hello, World!")
+
 
 class TestHelloWorld2(unittest.TestCase):
 
